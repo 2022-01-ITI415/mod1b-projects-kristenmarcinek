9f216f41225b9d2122afbbfc51e4d8871385b676
Kristen <kristen.marcinek@rutgers.edu>
2022-02-03 16:20:39 -0500

    set high score (might be broken?)

--- Assets/01-Apple Picker/Scripts/ApplePicker.cs
+++ Assets/01-Apple Picker/Scripts/ApplePicker.cs
@@ -11,16 +11,20 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        basketList = new List<GameObject>();
+
         for( int i=0; i < numBaskets; i++ ) {
             GameObject tBasketGO = Instantiate(basketPrefab) as GameObject;
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
 
     }
@@ -34,6 +38,17 @@ public class ApplePicker : MonoBehaviour
         foreach (GameObject tGO in tAppleArray) {
             Destroy(tGO);
         }
+
+        // Destroy one of the baskets
+        // Get the index of the last Basket in basketList
+        int basketIndex = basketList.Count-1;
+
+        // Get a reference to that Basket GameObject
+        GameObject tBasketGO = basketList[basketIndex];
+
+        // Remove the basket from the list and destory the gameobject
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
 
     }
 
