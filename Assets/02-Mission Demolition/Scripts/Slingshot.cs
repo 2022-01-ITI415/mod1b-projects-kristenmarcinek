930aed143e5052270ebcc924888ea3285983bf00
Kristen <kristen.marcinek@rutgers.edu>
2022-02-10 20:02:52 -0500

    change slingshot script

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class Slingshot : MonoBehaviour
 {
 
+    // fields set in the Unity Inspector pane
+    public GameObject prefabProjectile; 
+    public bool ____________;
+    // fields set dynamically
     public GameObject launchPoint;
-
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+    
     void Awake()
     {
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
     }
 
     void OnMouseEnter()
@@ -24,6 +32,16 @@ public class Slingshot : MonoBehaviour
     {
         // print("Slingshot:OnMouseExit()");
         launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        // the player has pressed the mouse button while over slingshot
+        aimingMode = true;
+        // instantiate a projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        // start it at launchpoint
+        projectile.transform.position = launchPos;
     }
 
     // Start is called before the first frame update
