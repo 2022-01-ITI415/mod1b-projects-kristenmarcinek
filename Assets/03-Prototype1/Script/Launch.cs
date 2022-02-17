390299891c8a0a89e3e1d924c500a329241afecd
Kristen <kristen.marcinek@rutgers.edu>
2022-02-17 18:29:47 -0500

    launch script

---
+++ Assets/03-Prototype1/Script/Launch.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launch : MonoBehaviour
+{
+
+    void OnMouseEnter()
+    {
+        print("Launch:OnMouseEnter()");
+    }
+
+    void OnMouseExit()
+    {
+        print("Launch:OnMouseExit()");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
