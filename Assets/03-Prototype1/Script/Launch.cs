42ade8b247b85d43296d147b6845da57608a4851
Kristen <kristen.marcinek@rutgers.edu>
2022-02-17 18:41:17 -0500

    get rid of launch

--- Assets/03-Prototype1/Script/Launch.cs
+++ Assets/03-Prototype1/Script/Launch.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class Launch : MonoBehaviour
 {
 
+    public GameObject launchPoint;
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+
     void OnMouseEnter()
     {
-        print("Launch:OnMouseEnter()");
+        // print("Launch:OnMouseEnter()");
+        launchPoint.SetActive(true);
     }
 
     void OnMouseExit()
     {
-        print("Launch:OnMouseExit()");
+        // print("Launch:OnMouseExit()");
+        launchPoint.SetActive(false);
     }
 
     // Start is called before the first frame update
