72c9e4c24babf87e79829db93b95813e952aea35
Kristen <kristen.marcinek@rutgers.edu>
2022-02-22 19:26:46 -0500

    made followcam script

---
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+
+    static public GameObject POI; // the static point of interest
+
+    [Header("Set Dynamically")]
+    public float camZ; // the desired z pos of the camera
+
+    void Awake()
+    {
+        camZ = this.transform.position.z;
+    }
+
+    void FixedUpdate()
+    {
+        // if there's only one line following an if, it doesn't need braces
+        if (POI == null) return; // return if there is no poi
+
+        // get the position of the poi
+        Vector3 destination = POI.transform.position;
+        // force the destination.z to be camZ to keep the camera far enough away
+        destination.z = camZ;
+        // set the camera to the destination
+        transform.position = destination;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
