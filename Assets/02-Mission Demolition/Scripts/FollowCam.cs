566e145f105e22834a60c75eaa3d3f300fd3eed4
Kristen <kristen.marcinek@rutgers.edu>
2022-02-22 19:40:05 -0500

    tweak camera

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -7,6 +7,10 @@ public class FollowCam : MonoBehaviour
 
     static public GameObject POI; // the static point of interest
 
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
     [Header("Set Dynamically")]
     public float camZ; // the desired z pos of the camera
 
@@ -22,10 +26,17 @@ public class FollowCam : MonoBehaviour
 
         // get the position of the poi
         Vector3 destination = POI.transform.position;
+        // limit the x & y to minimum values
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        // interpolate from the current camera position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
         // force the destination.z to be camZ to keep the camera far enough away
         destination.z = camZ;
         // set the camera to the destination
         transform.position = destination;
+        // set the orthographicSize of the Camera to keep Ground in view
+        Camera.main.orthographicSize = destination.y + 10;
     }
 
     // Start is called before the first frame update
